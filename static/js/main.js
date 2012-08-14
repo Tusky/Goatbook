51426c374f061d75a049465b71ad6ffa72bc39c7
Tusky <tuskone16@gmail.com>
2012-08-14 20:17:30 +0200

    ajax ready searchbox

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
     $('#profile').click(function(){
-        $('.triangle_down, #profile').toggleClass('active');
-        $('#profile_options').toggle();
+        $('.triangle_down, #profile').toggleClass('active')
+        $('#profile_options').toggle()
+    })
+
+    $('#search_box').keyup(function(){
+        var len=$(this).val().length
+        if( len > 0){
+            $('#search_results').show()
+        }else{
+            $('#search_results').hide()
+        }
     })
 })
