16129e22be0778a48189f07cf06b9293973055fc
Richard Hajdu <tuskone16@gmail.com>
2012-08-26 21:16:49 +0200

    wall textarea autosize added

--- wall/forms.py
+++ wall/forms.py
@@ -4,8 +4,4 @@ class WallPostForm(forms.Form):
     wallpost         = forms.CharField(label="", widget=forms.Textarea(attrs={
                                                                                 'autofocus':'autofocus',
                                                                                 'placeholder': 'What\'s on your mind?',
-                                                                                'autocomplete': 'off',
-                                                                                'aria-autocomplete': 'list',
-                                                                                'aria-expanded': 'false',
-                                                                                'aria-invalid': 'false',
-                                                                              }))
+                                                                                'autocomplete': 'off'}))
